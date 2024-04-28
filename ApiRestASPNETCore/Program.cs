406b7931fdd96cfb88b8fb51d66d4e172734a80e
Malinowsk <juanrago2015@gmail.com>
2024-04-28 19:22:40 -0300

    Se elimina la carpeta view

--- ApiRestASPNETCore/Program.cs
+++ ApiRestASPNETCore/Program.cs
@@ -21,14 +21,8 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build(); // se construye la app
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-
-}
-else {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
